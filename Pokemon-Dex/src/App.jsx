214759ee8d4f55058b4b01021c228581c6c800d3
hilal <hilalhidayah589456@gmail.com>
2025-02-02 21:28:49 +0700

    feat: refactor layout structure; integrate sidebar and fetch Pokémon data for HomePage

--- Pokemon-Dex/src/App.jsx
+++ Pokemon-Dex/src/App.jsx
@@ -1,15 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
-import AppSideBar from "./components/AppSideBar";
 
 function App() {
   return (
     <>
-      <SidebarProvider>
-        <AppSideBar />
-
-        <Outlet />
-      </SidebarProvider>
+      <Outlet />
     </>
   );
 }
