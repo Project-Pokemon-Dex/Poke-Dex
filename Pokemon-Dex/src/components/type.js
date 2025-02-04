574675564df1640fa4a981fbafd6b36d5c043485
hilal <hilalhidayah589456@gmail.com>
2025-02-04 22:30:57 +0700

    Update PostCSS configuration and add new type icons for improved styling and functionality

---
+++ Pokemon-Dex/src/components/type.js
@@ -0,0 +1,118 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import bug from "../assets/type-icons/bug.svg";
+import dark from "../assets/type-icons/dark.svg";
+import dragon from "../assets/type-icons/dragon.svg";
+import electric from "../assets/type-icons/electric.svg";
+import fairy from "../assets/type-icons/fairy.svg";
+import fighting from "../assets/type-icons/fighting.svg";
+import fire from "../assets/type-icons/fire.svg";
+import flying from "../assets/type-icons/flying.svg";
+import ghost from "../assets/type-icons/ghost.svg";
+import grass from "../assets/type-icons/grass.svg";
+import ground from "../assets/type-icons/ground.svg";
+import ice from "../assets/type-icons/ice.svg";
+import normal from "../assets/type-icons/normal.svg";
+import poison from "../assets/type-icons/poison.svg";
+import psychic from "../assets/type-icons/psychic.svg";
+import rock from "../assets/type-icons/rock.svg";
+import steel from "../assets/type-icons/steel.svg";
+import water from "../assets/type-icons/water.svg";
+
+const type = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Bug",
+    url: "#",
+    icon: bug,
+  },
+  {
+    title: "Dark",
+    url: "#",
+    icon: dark,
+  },
+  {
+    title: "Dragon",
+    url: "#",
+    icon: dragon,
+  },
+  {
+    title: "Electric",
+    url: "#",
+    icon: electric,
+  },
+  {
+    title: "Fairy",
+    url: "#",
+    icon: fairy,
+  },
+  {
+    title: "Fighting",
+    url: "#",
+    icon: fighting,
+  },
+  {
+    title: "Fire",
+    url: "#",
+    icon: fire,
+  },
+  {
+    title: "Flying",
+    url: "#",
+    icon: flying,
+  },
+  {
+    title: "Ghost",
+    url: "#",
+    icon: ghost,
+  },
+  {
+    title: "Grass",
+    url: "#",
+    icon: grass,
+  },
+  {
+    title: "Ground",
+    url: "#",
+    icon: ground,
+  },
+  {
+    title: "Ice",
+    url: "#",
+    icon: ice,
+  },
+  {
+    title: "Normal",
+    url: "#",
+    icon: normal,
+  },
+  {
+    title: "Poison",
+    url: "#",
+    icon: poison,
+  },
+  {
+    title: "Psychic",
+    url: "#",
+    icon: psychic,
+  },
+  {
+    title: "Rock",
+    url: "#",
+    icon: rock,
+  },
+  {
+    title: "Steel",
+    url: "#",
+    icon: steel,
+  },
+  {
+    title: "Water",
+    url: "#",
+    icon: water,
+  },
+];
+export default type;
--- Pokemon-Dex/src/pages/HomePage.jsx
+++ Pokemon-Dex/src/pages/HomePage.jsx
@@ -8,38 +8,69 @@ import CardItems from "@/components/CardItems";
 const HomePage = () => {
   // fetch data
   const [getData, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+
+  const getHomeData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?limit=500&offset=20`
+      );
+      const allData = data.results;
+      // karena fetch data api bentuk promise jadi pakai konsep promise all untuk kecepatan data
+      const detailedData = await Promise.all(
+        allData.map(async (item) => {
+          const response = await axios.get(item.url);
+          return response.data;
+        })
+      );
+      setData(detailedData);
+      setFilterData(detailedData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Mencari data dari query
+  const searchFeat = (query) => {
+    if (!query) {
+      getHomeData(); // Jika input kosong, tampilkan semua data
+      return;
+    }
+
+    const filteredData = filterData.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    setData(filteredData);
+  };
+
+  // mencari berdasarkan types
+  const filterPokemon = (typeValue) => {
+    const filterByType = filterData.filter((pokemon) =>
+      pokemon.types.some(
+        (t) => t.type.name.toLowerCase() === typeValue.toLowerCase()
+      )
+    );
+    setData(filterByType);
+    console.log(getData);
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
-        const allData = data.results;
-        // karena fetch data api bentuk promise jadi pakai konsep promise all untuk kecepatan data
-        const detailedData = await Promise.all(
-          allData.map(async (item) => {
-            const response = await axios.get(item.url);
-            return response.data;
-          })
-        );
-        setData(detailedData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData();
+    getHomeData();
   }, []);
+
   return (
     <>
-    <main>
-      <Navbar />
-      <SidebarProvider className="w-full flex-row relative">
-        <AppSideBar />
-        <div className="grid grid-cols-4 gap-2 w-full p-2 bg-[#1A1A1D]">
-          {getData &&
-          getData.map((items, i) => <CardItems key={i} item={items} />)}
-        </div>
-      </SidebarProvider>
-    </main>
+      <main>
+        <Navbar searchFeat={searchFeat} />
+        <SidebarProvider className="w-full flex-row relative">
+          <AppSideBar filterSearch={filterPokemon} />
+          <div className="grid grid-cols-4 gap-2 w-full p-2 bg-[#1A1A1D]">
+            {getData &&
+              getData.map((items, i) => <CardItems key={i} item={items} />)}
+          </div>
+        </SidebarProvider>
+      </main>
     </>
   );
 };
