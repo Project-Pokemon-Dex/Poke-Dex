1ae3621888c798aaceae98ea5fc7ef1921890069
delsfi <aloisiusfidel1@gmail.com>
2025-02-06 22:41:41 +0700

    Responsive

--- Pokemon-Dex/src/pages/DetailPage.jsx
+++ Pokemon-Dex/src/pages/DetailPage.jsx
@@ -117,27 +117,24 @@ const DetailPage = () => {
 
   return (
     <div
-      className="p-10 min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center"
+      className="p-4 sm:p-10 min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center"
       onClick={handleClick} // Memanggil handleClick untuk semua klik di halaman
     >
       {/* Pokémon Details */}
-      <div className="bg-[#1A1A1D] p-6 rounded-xl shadow-xl w-[900px] flex flex-col items-center gap-6 border border-gray-600">
-        <div className="w-full flex gap-20">
+      <div className="bg-[#1A1A1D] p-6 rounded-xl shadow-xl max-w-[900px] w-full flex flex-col items-center gap-6 border border-gray-600">
+        <div className="w-full flex flex-col md:flex-row gap-6 md:gap-20">
           <div className="flex flex-col">
             <div className="flex items-start gap-3 px-2 py-1">
               {/* Back Button */}
-              <Link
-                to="/"
-                className=" pt-2 text-white hover:text-yellow-400"
-              >
+              <Link to="/" className="pt-2 text-white hover:text-yellow-400">
                 <CircleArrowLeft size={24} />
               </Link>
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png"
-                style={{ width: "40px", height: "40px" }}
+                className="w-10 h-10"
                 alt=""
               />
-              <div className="">
+              <div>
                 <h1 className="text-2xl font-bold capitalize">
                   {pokemon.name}
                 </h1>
@@ -159,57 +156,53 @@ const DetailPage = () => {
             </div>
           </div>
 
-          <div className="w-1/2 flex justify-center items-center relative">
+          <div className="w-full md:w-1/2 flex justify-center items-center relative">
             <img
               src={pokemon.imageUrl}
               alt={pokemon.name}
-              className="h-80 object-contain"
+              className="h-60 sm:h-80 object-contain"
             />
           </div>
 
-          <div className="border border-gray-600 rounded-lg p-2">
-            <div className="flex flex-col gap-2">
-              <div className="bg-green-700 p-2 rounded-lg">
-                <p className="text-white text-sm font-medium flex gap-2 px-1 py-2">
-                  <RulerIcon size={20} />
-                  Height
-                </p>
-                <p className="text-lg font-semibold px-1 py-1">
-                  {pokemon.height}
-                </p>
-              </div>
-              <div className="bg-green-700 p-3 rounded-lg">
-                <p className="text-white text-sm font-medium flex gap-2 px-1 py-2">
-                  <WeightIcon size={20} />
-                  Weight
-                </p>
-                <p className="text-lg font-semibold px-1 py-1">
-                  {pokemon.weight}
-                </p>
-              </div>
-              <div className="bg-green-700 p-3 rounded-lg">
-                <p className="text-white text-sm font-medium flex gap-2 px-1 py-2 truncate">
-                  <StarIcon size={20} />
-                  Gender Ratio
-                </p>
-                <p className="text-lg font-semibold">{pokemon.genderRatio}</p>
-              </div>
-
-              <div className="bg-green-700 p-3 rounded-lg">
-                <p className="text-white text-sm font-medium flex gap-2 px-1 py-2">
-                  <EggIcon size={20} />
-                  Egg Cycles
-                </p>
-                <p className="text-lg font-semibold px-1 py">
-                  {pokemon.hatchCycles}
-                </p>
-              </div>
+          <div className="border border-gray-600 rounded-lg p-2 w-full md:w-auto">
+            <div className="flex flex-col gap-2 truncate ">
+              {[
+                {
+                  icon: <RulerIcon size={20} />,
+                  label: "Height",
+                  value: pokemon.height,
+                },
+                {
+                  icon: <WeightIcon size={20} />,
+                  label: "Weight",
+                  value: pokemon.weight,
+                },
+                {
+                  icon: <StarIcon size={20} />,
+                  label: "Gender Ratio",
+                  value: pokemon.genderRatio,
+                },
+                {
+                  icon: <EggIcon size={20} />,
+                  label: "Egg Cycles",
+                  value: pokemon.hatchCycles,
+                },
+              ].map((item, index) => (
+                <div key={index} className="bg-green-700 p-2 rounded-lg">
+                  <p className="text-white text-sm font-medium flex gap-2 px-1 py-2">
+                    {item.icon} {item.label}
+                  </p>
+                  <p className="text-lg font-semibold px-1 py-1">
+                    {item.value}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
 
         {/* Catch Button */}
-        <button className="button truncate font-bold">
+        <button className="button truncate font-bold w-full sm:w-auto">
           <div>
             <span>Catch Pokemon</span>
           </div>
@@ -217,25 +210,25 @@ const DetailPage = () => {
       </div>
 
       {/* Evolution Chain */}
-      <div className="w-[900px] mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
-        <div className="flex items-center justify-center gap-4">
+      <div className="max-w-[900px] w-full mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-6">
           {evolutionChain.map((evo, index) => (
-            <div key={index} className="flex items-center gap-4">
+            <div
+              key={index}
+              className="flex flex-col sm:flex-row items-center gap-4"
+            >
               <div className="border border-[#ffcb05] p-3 rounded-lg flex flex-col items-center hover:scale-110 transition duration-500">
-                {/* Membungkus gambar dengan Link */}
                 <Link to={`/pokemon/${evo.name}`}>
                   <img
                     src={evo.imageUrl}
                     alt={evo.name}
-                    className="w-20 h-20 object-contain"
+                    className="w-16 h-16 sm:w-20 sm:h-20 object-contain"
                   />
                 </Link>
-                <p className="font-bold capitalize">{evo.name}</p>
+                <p className="font-bold capitalize text-center">{evo.name}</p>
               </div>
-
               {index < evolutionChain.length - 1 && (
-                <div className="flex flex-col items-center text-white">
-                  <p className="text-sm text-gray-400"></p>
+                <div className="flex flex-col sm:flex-row items-center text-white">
                   <div className="px-2 py-1 bg-gray-500 rounded-md">
                     Lv. {evolutionChain[index + 1].minLevel}
                   </div>
@@ -247,8 +240,8 @@ const DetailPage = () => {
       </div>
 
       {/* Stats */}
-      <div className=" flex gap-5">
-        <div className="w-[630px] mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
+      <div className="flex flex-col md:flex-row gap-5 max-w-[900px] w-full">
+        <div className="w-full md:w-2/3 mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
           <h2 className="text-xl font-semibold mb-4">Stats</h2>
           <div className="space-y-2">
             {pokemon.stats.map((stat, index) => (
@@ -257,14 +250,12 @@ const DetailPage = () => {
                   {stat.name.replace(/-/g, " ")}
                 </p>
                 <div className="flex items-center w-3/4">
-                  {/* Bar chart */}
                   <div className="w-full bg-gray-700 h-3 rounded-full overflow-hidden mr-2">
                     <div
                       className="bg-[#ffcb05] h-full"
                       style={{ width: `${(stat.value / 255) * 100}%` }}
                     />
                   </div>
-                  {/* Stat value */}
                   <p className="text-white">{stat.value}</p>
                 </div>
               </div>
@@ -273,39 +264,28 @@ const DetailPage = () => {
         </div>
 
         {/* Additional Info */}
-        <div className=" bg-[#1A1A1D] mt-6 p-6 rounded-xl shadow-xl border border-gray-600">
+        <div className="bg-[#1A1A1D] mt-6 p-6 rounded-xl shadow-xl border border-gray-600 w-full md:w-1/3">
           <h3 className="text-xl font-semibold">Additional Info</h3>
           <ul className="space-y-4 mt-4 text-white font-medium">
-            <li>
-              <strong className="text-gray-400">Generation:</strong>{" "}
-              {pokemon.generation}
-            </li>
-            <li>
-              <strong className="text-gray-400">Habitat:</strong>{" "}
-              {pokemon.habitat}
-            </li>
-            <li>
-              <strong className="text-gray-400">Growth Rate:</strong>{" "}
-              {pokemon.growthRate}
-            </li>
-            <li>
-              <strong className="text-gray-400">Base Experience:</strong>{" "}
-              {pokemon.baseExp}
-            </li>
-            <li>
-              <strong className="text-gray-400">Capture Rate:</strong>{" "}
-              {pokemon.captureRate}
-            </li>
-            <li>
-              <strong className="text-gray-400">Base Happiness:</strong>{" "}
-              {pokemon.baseHappiness}
-            </li>
+            {[
+              { label: "Generation", value: pokemon.generation },
+              { label: "Habitat", value: pokemon.habitat },
+              { label: "Growth Rate", value: pokemon.growthRate },
+              { label: "Base Experience", value: pokemon.baseExp },
+              { label: "Capture Rate", value: pokemon.captureRate },
+              { label: "Base Happiness", value: pokemon.baseHappiness },
+            ].map((item, index) => (
+              <li key={index}>
+                <strong className="text-gray-400">{item.label}:</strong>{" "}
+                {item.value}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
-      <div className="w-[900px] mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
+      <div className="max-w-[900px] w-full mt-6 bg-[#1A1A1D] p-4 rounded-lg border border-gray-600">
         <h3 className="text-xl font-semibold mb-4">Abilities</h3>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {abilities.map((ability, index) => (
             <div
               key={index}
